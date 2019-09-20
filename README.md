9602b7a7a19d7af1e13e67bbec845638624fe6a1
Anees Hameed <aneesshameed@gmail.com>
2019-09-21 03:02:47 +0530

    Inital commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# \<app-cookies\>
+
+### Element to manage cookies across your app. app-cookies read values from cookie data automatically and serves it to client. Settting and resetting of values are not automatic and need to be triggered by code.
+
+## Installation
+
+```
+npm install --save @elifent/app-cookies
+```
+
+## In an html file
+
+```html
+<html>
+  <head>
+    <script type="module">
+      import "@elifent/app-cookies/app-cookies.js";
+    </script>
+  </head>
+  <body>
+    <app-cookies key="location" data="US"></app-cookies>
+  </body>
+</html>
+```
+
+## In a Polymer 3 element
+
+```js
+import { PolymerElement, html } from "@polymer/polymer";
+import "@elifent/app-cookies/app-cookies.js";
+class SampleElement extends PolymerElement {
+  static get template() {
+    return html`
+      <app-cookies id="location" key="location" data="{{location}}"></app-cookies>
+      <span>{{location}}</span>
+      <input type="text" value="{{newvalue}}">
+      <input type="button" value"Set Location" on-click="set">
+      <input type="button" value="Unset Location" on-click="unset">
+    `;
+  }
+
+  static get properties() {
+    return {
+      location: {
+        type: String,
+        value: "US"
+      },
+      newvalue:{
+        type: String,
+        value: null
+      }
+    };
+  }
+
+  setLocation(){
+      this.$.location.set(this.newvalue);
+  }
+
+  unsetLocation(){
+      this.$.location.unset();
+  }
+
+
+}
+customElements.define("sample-element", SampleElement);
+```
+
+### Installation
+
+```
+  git clone https://github.com/elifent/app-cookies
+  cd paper-avatar
+  npm install
+  npm install -g polymer-cli
+```
+
+### Running the demo locally
+
+```
+  polymer serve --npm
+  open http://127.0.0.1:<port>/components/app-cookies/demo/
+
+```
+
+Found issues? Let me know
---
+++ app-cookies.js
@@ -0,0 +1,109 @@
+import PolymerElement from "@polymer/polymer/polymer-element.js";
+import cookiejs from "cookiejs.js";
+
+/**
+ * `app-cookies`
+ * Polymer element to work with browser cookies
+ * app-cookies read values from cookie data automatically and serves it to client. Settting and resetting of values are not automatic and need to be triggered by code.
+ *
+ * Add app-cookies to code as below
+ * <app-cookies id="elementId" key="cookieKey"></app-cookies>
+ * 
+ * To set app cookies use
+ * this.$.elementId.set('new value');
+ * 
+ * To unset value of cookie use
+ * this.$.elementId.unset();
+ * 
+ * If you want to read values from app-cookies, code as below
+ * <app-cookies key="cookieKey" data="{{cookieValue}}"></app-cookies>
+ * 
+ * 
+ * @customElement
+ * @polymer
+ */
+class AppCookies extends PolymerElement {
+
+  static get properties() {
+    return {
+      /** Cookie key*/
+      key: {
+        type: String,
+        value: null
+      },
+      /** Cookie data*/
+      data: {
+        type: String,
+        value: null,
+        notify: true
+      },
+      /** Options that can be used while setting cookie 
+       * available options are
+       * 1. domain
+       * Specifies allowed hosts to receive the cookie, default is the host of the current document location
+       * eg: 'http://elifent.tech'
+       * 
+       * 2. path
+       * Indicates a URL path that must exist in the requested URL, default is '/'
+       * 
+       * 3. expires
+       * Cookie is deleted when the client shuts down (session-cookie) or when the expire date is reached, default is after sessio is closed
+       * 
+       * 4. max-age
+       * Cookie expires after a specific length of time in seconds, default is undefined
+       * 
+       * 5. secure
+       * Cookie is only sent to the server with a encrypted request over the HTTPS protocol, default: false
+      */
+      options: {
+        type: Object,
+        value: {}
+      }
+    };
+  }
+
+  ready(){
+    super.ready();
+    //When ready load value from cookiejs to this.data
+    this.data = cookiejs.get(this.key);
+  }
+
+  set(value) {
+    //Creating a new cookie
+    //Set option to null and then each value to option if they are present in properties
+    //If not present cookiejs will use the default value
+
+    let options = {};
+    
+    if(this.options['domain'] != undefined){
+        options['domain'] = this.options['domain'];
+    }
+    
+    if(this.options['path'] != undefined){
+        options['path'] = this.options['path'];
+    }
+
+    if(this.options['expires'] != undefined){
+        options['path'] = this.options['expires'];
+    }
+
+    if(this.options['max-age'] != undefined){
+        options['max-age'] = this.options['max-age'];
+    }
+
+    options['secure'] = this.options['secure'];
+
+    //Create a new cookie
+    cookiejs.set(this.key, value);
+     //mutate the value to data
+    this.data = value;
+  }
+
+  unset() {
+    //Remove cookie from cookiejs
+    cookiejs.remove(this.key);
+    this.data = null;
+  }
+}
+
+window.customElements.define("app-cookies", AppCookies);
